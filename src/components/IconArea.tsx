d5dbcb321f29e38e249f5e4f2050db505c45c968
Jason <jason.kim@ambitai.com>
2024-10-08 15:16:05 +1300

    icon area color

--- src/components/IconArea.tsx
+++ src/components/IconArea.tsx
@@ -1,6 +1,6 @@
 export const IconArea = () => {
   return (
-    <div className="w-16 p-4 flex items-start justify-center">
+    <div className="w-16 p-4 flex items-start justify-center bg-gray-100">
       <img
         src="https://momentum-clone-jason.netlify.app/images/news-icon.png"
         alt="Icon"
